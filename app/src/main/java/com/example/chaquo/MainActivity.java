3e4a60b4709caa9a5ccd36d4cf76ca148c8f70d6
MrShakila <shakilaadi.es@gmail.com>
2022-12-28 11:38:02 +0530

    first commit

---
+++ app/src/main/java/com/example/chaquo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.chaquo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+    EditText num1,num2;
+    Button btn;
+    TextView txt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        num1 =(EditText) findViewById(R.id.num1);
+        num2 =(EditText) findViewById(R.id.num2);
+        btn =(Button) findViewById(R.id.btn);
+        txt =(TextView) findViewById(R.id.textView);
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+            System.out.println("python script running");
+        }
+        Python py = Python.getInstance();
+        PyObject pyObject = py.getModule("script");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PyObject obj = pyObject.callAttr("main",num1.getText().toString(),num2.getText().toString());
+                txt.setText(obj.toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/python/script.py
@@ -0,0 +1,6 @@
+def main(num1, num2):
+    num3 = int(num1)
+    num4 = int(num2)
+    result = num3 + num4
+
+    return str(result)
