8e6019b893075525d015fa0efc30272123502cf6
MrShakila <shakilaadi.es@gmail.com>
2022-12-30 12:51:17 +0530

    blutooth search added

--- app/src/main/java/com/example/chaquo/MainActivity.java
+++ app/src/main/java/com/example/chaquo/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.chaquo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +25,12 @@ public class MainActivity extends AppCompatActivity {
     EditText num1,num2;
     Button btn;
     TextView txt;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothLeScanner scanner;
+    private EEGReceiver receiver;
 
+    private BluetoothDevice museDevice;
+   // TextView museview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
         txt =(TextView) findViewById(R.id.textView);
         num1.setVisibility(View.INVISIBLE);
         num2.setVisibility(View.INVISIBLE);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        scanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        // Create the EEG receiver
+        receiver = new EEGReceiver(this);
+
+        // Start scanning for BLE devices
+
+        scanner.startScan(scanCallback);
+
 
         if (! Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
@@ -40,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     Python py = Python.getInstance();
                     PyObject pyObject = py.getModule("script");
-                    PyObject obj = pyObject.callAttr("create_stream");
+                    PyObject obj = pyObject.callAttr("main");
                     txt.setText(obj.toString());
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -49,5 +73,206 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            // Check if the device is the desired BLE device
+            System.out.println(device.getName());
+            if (device.getName().contains("Muse")) {
+                // Stop scanning
+               // museview.setText("Muse Found");
+                scanner.stopScan(scanCallback);
+                // Connect to the device and start receiving data
+                connectToDevice(device);
+            }
+        }
+
+
+        // Method to connect to the BLE device and start receiving data
+        private void connectToDevice(BluetoothDevice device) {
+            // Connect to the BLE device
+            BluetoothGatt gatt = device.connectGatt(getApplicationContext(), false, gattCallback);
+          //  museview.setText("Muse Connected");
+            connectToMuse(device);
+            // Enable notifications for the EEG data characteristic
+//        BluetoothGattService service = gatt.getService(UUID.fromString("0000aa10-0000-1000-8000-00805f9b34fb"));
+//        BluetoothGattCharacteristic characteristic = service.getCharacteristic(UUID.fromString("0000aa11-0000-1000-8000-00805f9b34fb"));
+//        gatt.setCharacteristicNotification(characteristic, true);
+//        BluetoothGattDescriptor descriptor = characteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+//        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+//        gatt.writeDescriptor(descriptor);
+        }
+
+        // Callback for GATT events
+        private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+            @Override
+            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    // Discover services
+                    gatt.discoverServices();
+                }
+            }
+
+            @Override
+            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                // Services have been discovered
+            }
+
+            @Override
+            public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                // EEG data characteristic has changed
+                byte[] data = characteristic.getValue();
+                // Process the data here
+                receiver.processData(data);
+            }
+        };
+
+        // Method to connect to the Muse 2 headband and start receiving data
+//            private void connectToMuse() {
+//                // Find the Muse 2 headband's EEG stream
+//                LSL.StreamInfo[] results = LSL.resolve_streams();
+//                LSL.StreamInfo info = results;
+//
+//                // Open the EEG stream
+//                LSL.StreamInlet inlet = null;
+//                try {
+//                    inlet = new LSL.StreamInlet(info);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                // Create a buffer to hold the sample data
+//                //double[] sample = new double[inlet.channel_count()];
+//
+//                // Start a thread to receive data from the Muse 2 headband
+////                new Thread(() -> {
+////                    while (true) {
+////                        // Pull a sample from the stream
+////                        double timestamp = inlet.pull_sample(sample);
+////
+////                        // Process the sample data
+////                        processData(timestamp, sample);
+////                    }
+////                }).start();
+//            }
+
+        // Method to process the data received from the Muse 2 headband
+        private void processData(double timestamp, double[] channels) {
+            // Calculate the alpha activity from the AF7 and AF8 channels
+            double alpha = (channels[1] + channels[2]) / 2;
+
+            // Do something with the alpha data
+            // ...
+        }
+
+        private void connectToMuse(BluetoothDevice device) {
+            // Initialize the Bluetooth adapter
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            museDevice = device;
+
+            // Check if Bluetooth is supported
+//            if (bluetoothAdapter == null) {
+//                Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
+//                return;
+//            }
+//
+//            // Check if Bluetooth is enabled
+//            if (!bluetoothAdapter.isEnabled()) {
+//                // Request Bluetooth permission
+//                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//                return;
+//            }
+//
+//            // Start a thread to scan for the Muse 2 headband
+
+            // Get the list of paired devices
+
+
+            // Find the Muse 2 headba
+
+
+
+            // Connect to the Muse 2 headband
+            if (museDevice != null) {
+              //  String streamName = findEEGStream(museDevice);
+               // System.out.println(streamName);
+//                    private void getAlphaData(BluetoothDevice museDevice) {
+//
+//                        // Create an inlet for the EEG stream provided by the headband
+//                        LSL.StreamInlet inlet = LSL.StreamInlet.open_by_bluetooth_device(LSL.StreamInfo.from_bluetooth_device(museDevice));
+//
+//                        // Create a buffer to hold the sample data
+//                        double[] sample = new double[inlet.channel_count()];
+//
+//                        // Start a thread to receive data from the Muse 2 headband
+//                        new Thread(() -> {
+//                            while (true) {
+//                                // Pull a sample from the stream
+//                                double timestamp = inlet.pull_sample(sample);
+//
+//                                // Calculate the alpha activity from the AF7 and AF8 channels
+//                                double alpha = (sample[1] + sample[2]) / 2;
+//
+//                                // Do something with the alpha data
+//                                processAlpha(timestamp, alpha);
+//                            }
+//                        }).start();
+//                    }
+
+                // Method to process the alpha data received from the Muse 2 headband
+//                    private void processAlpha(double timestamp, double alpha) {
+//                        // Do something with the alpha data
+//                        // For example, update the UI with the alpha value
+//                        runOnUiThread(() -> {
+//                            TextView alphaTextView = findViewById(R.id.alpha_text_view);
+//                            alphaTextView.setText(String.format("Alpha: %.2f", alpha));
+//                        });
+//
+//                        // Alternatively, you could save the alpha data to a file
+//                        // ...
+//                    }
+                // Find the EEG stream provided by the headband
+                //   LSL.StreamInfo[] results = LSL.resolve_stream("bluetooth_device_address", museDevice.getAddress(), 1, LSL.FOREVER);
+                // LSL.StreamInfo info = results[0];
+                //  System.out.println(results);
+//                    System.out.println(info);
+//                    // Open the EEG stream
+//                    try {
+//                        LSL.StreamInlet inlet = new LSL.StreamInlet(info);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    // Create a buffer to hold the sample data
+//                //    double[] sample = new double[inlet.pull_sample(2,100)];
+//
+//                    // Start a thread to receive data from the Muse 2 headband
+//                    new Thread(() -> {
+//                        while (true) {
+//                            // Pull a sample from the stream
+//                            double timestamp = 0;
+//                            try {
+//                             //   timestamp = inlet.pull_sample(sample);
+//                            } catch (Exception e) {
+//                                e.printStackTrace();
+//                            }
+//
+//                            // Calculate the alpha activity from the AF7 and AF8 channels
+//                          //  double alpha = (sample[1] + sample[2]) / 2;
+//
+//                            // Do something with the alpha data
+//                        //    processAlpha(timestamp, alpha);
+//                        }
+//                    }).start();
+            } else {
+                // Muse 2 headband not found
+                //  Toast.makeText(this, "Muse 2 headband not found", Toast.LENGTH_SHORT).show();
+            }
+
+
+        };
+    };
 }
 
--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -1,12 +1,84 @@
-import pylsl
+#!/usr/bin/env python3
+#---------------------------------------------------
 
-def create_stream():
-    # Create the stream info
-    stream_info = pylsl.StreamInfo('My Stream', 'EEG', 4, 100, pylsl.cf_float32, 'myuniqueid23443')
-    # Create the outlet
-    outlet = pylsl.StreamOutlet(stream_info)
-    return outlet
+import numpy as np  # Module that simplifies computations on matrices
+from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
 
-# Test the function
-outlet = create_stream()
-print(outlet)
+
+
+# Handy little enum to make code more readable
+
+
+def main():
+
+        """ EXPERIMENTAL PARAMETERS """
+        # Modify these to change aspects of the signal processing
+
+        # Length of the EEG data buffer (in seconds)
+        # This buffer will hold last n seconds of data and be used for calculations
+        BUFFER_LENGTH = 3
+
+        # Length of the epochs used to compute the FFT (in seconds)
+        EPOCH_LENGTH = 1
+
+        # Amount of overlap between two consecutive epochs (in seconds)
+        OVERLAP_LENGTH = 0.8
+
+        # Amount to 'shift' the start of each next consecutive epoch
+        SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+
+        #num of blink in the stream
+        blink_count = 0
+        t = 0
+
+        d = 0.7 #trough distance
+        h = -90  #trough height
+
+        # Index of the channel(s) (electrodes) to be used
+        # 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
+        #EYE_CHANNEL = [0]
+        CON_CHANNEL = [3]
+
+        """ 1. CONNECT TO EEG STREAM """
+        # Search for active LSL streams
+        print('Looking for an EEG stream...')
+        streams = resolve_byprop('type', 'EEG', timeout=2)
+        if len(streams) == 0:
+            raise RuntimeError('Can\'t find EEG stream.')
+
+        # Set active EEG stream to inlet and apply time correction
+        print("Start acquiring data")
+        inlet = StreamInlet(streams[0], max_chunklen=12)
+        eeg_time_correction = inlet.time_correction()
+
+        # Get the stream info and description
+        info = inlet.info()
+        description = info.desc()
+
+        # Get the sampling frequency
+        # This is an important value that represents how many EEG data points are
+        # collected in a second. This influences our frequency band calculation.
+        # for the Muse 2016, this should always be 256
+        fs = int(info.nominal_srate())
+
+        """ 2. INITIALIZE BUFFERS """
+
+        # Initialize raw concentration EEG data buffer
+        con_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+        filter_state = None  # for use with the notch filter
+        eye_filter_state = None
+
+        eye_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+
+        # Compute the number of epochs in "buffer_length"
+        n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
+                                SHIFT_LENGTH + 1))
+
+        # Initialize the band power buffer (for plotting)
+        # bands will be ordered: [delta, theta, alpha, beta]
+        band_buffer = np.zeros((n_win_test, 4))
+
+        #intialize welch PSD
+        #self.welch_psd = utils.cal_welch_PSD(fs=self.fs)
+        return band_buffer
+
